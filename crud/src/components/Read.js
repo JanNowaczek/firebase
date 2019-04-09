48244301f3765a3854d53c51976f70f337dfe292
mrozo2m <mrozo2m@interia.pl>
2019-04-09 10:47:09 +0200

    component Read.js added

---
+++ crud/src/components/Read.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Read extends Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    Name: {}
+                </div>
+                <div>
+                    Age: {}
+                </div>
+                <div>
+                    Color: {}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Read
