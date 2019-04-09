e895b896b845c2c46a06f0c90adb322afa2faf42
mrozo2m <mrozo2m@interia.pl>
2019-04-09 09:41:20 +0200

    component List and Menu added

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Menu from './components/Menu'
+import List from './components/List'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <Menu />
+        <Route exact path="/" component={List} />
+      </BrowserRouter>
     );
   }
 }
---
+++ crud/src/components/List.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+
+  state = {
+    data: [
+      { id: 1, name: 'Lorem'}, { id: 2, name: 'Ipsum'}, { id: 3, name: 'Sit'}
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.data.map(elem => (
+          <div key={elem.id}>{elem.name}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default List;
