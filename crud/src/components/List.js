a530903b0dfa0d42112c43feea0ca4f4cf5f8397
mrozo2m <mrozo2m@interia.pl>
2019-04-09 10:30:47 +0200

    list of cats, componentDidMount added to List.js

--- crud/src/components/List.js
+++ crud/src/components/List.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 
 class List extends Component {
 
-  state = {
-    data: [
-      { id: 1, name: 'Lorem'}, { id: 2, name: 'Ipsum'}, { id: 3, name: 'Sit'}
-    ]
-  }
+    state = {
+        data: []
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.data.map(elem => (
-          <div key={elem.id}>{elem.name}</div>
-        ))}
-      </div>
-    );
-  }
+    componentDidMount() {
+        fetch('https://jfddl7-api-b832f.firebaseio.com/cats.json')
+            .then(response => response.json())
+            .then(responseData => {
+                const data = [];
+                Object.entries(responseData).forEach(elem => {
+                    data.push({
+                        id: elem[0],
+                        ...elem[1]
+                    });
+                });
+                this.setState({ data: data });
+                // this.setState({ data });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.data.map(elem => (
+                    <div key={elem.id}>{elem.name}</div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default List;
