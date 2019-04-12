f3e91a107fb680c0a480f0af9cef50ab5776d1b5
mrozo2m <mrozo2m@interia.pl>
2019-04-12 10:29:01 +0200

    MessageList and NewMessage.Form added

--- firebase4/chat/src/Chat.js
+++ firebase4/chat/src/Chat.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
+import MessagesList from './MessagesList'
+import NewMessageForm from './NewMessageForm'
+
 class Chat extends React.Component {
     state = {
         messages: null,
+        newMessageText: '',
     }
 
-    render(){
-        return(
+    onNewMessageTextChanged = (event) => this.setState({
+        newMessageText: event.target.value,
+    })
+
+    render() {
+        return (
             <div>
-                Chat
+                <MessagesList 
+                    messages={this.state.messages}
+                />
+                <NewMessageForm 
+                    newMessageText={this.state.newMessageText}
+                    onNewMessageTextChanged={this.onNewMessageTextChanged}
+                />
             </div>
         )
     }
