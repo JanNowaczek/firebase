0f862b18ce24ae94687cea64f8c254ae3c32738a
mrozo2m <mrozo2m@interia.pl>
2019-04-12 10:03:00 +0200

    firebaseConf added

---
+++ firebase4/chat/src/Auth.js
@@ -0,0 +1,55 @@
+
+import React from 'react'
+
+import { auth, googleProvider } from './firebaseConf'
+
+import Button from '@material-ui/core/Button'
+
+const styles = {
+    root: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {}
+}
+
+class Auth extends React.Component {
+    state = {
+        isUserLoggedIn: false,
+    }
+
+    onLogInByGoogleClick = () => {
+        auth.signInWithPopup(googleProvider)
+            .catch(console.log)
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.isUserLoggedIn ?
+                        this.props.children
+                        :
+                        <div
+                            style={styles.root}
+                        >
+                            <h1>jfddl7 Chat</h1>
+                            <Button
+                                variant={'contained'}
+                                color={'secondary'}
+                                onClick={this.onLogInByGoogleClick}
+                            >
+                                LOGIN BY GOOGLE
+                            </Button>
+
+                        </div>
+                }
+            </div>
+        )
+    }
+}
+export default Auth
---
+++ firebase4/chat/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+import App from './App'
+import Auth from './Auth'
+
+ReactDOM.render(
+    <Auth>
+        <App />
+    </Auth>,
+    document.querySelector('#root')
+)
